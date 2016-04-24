97ae185f76c2389f36821f66c5b697a3da35643b
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-04-24 15:13:45 -0600

    adding feature to the status.php page

--- status.php
+++ status.php
@@ -12,7 +12,7 @@
     ?>
   </head>
   <body>
-      
+
       <div class="pageContents">
 
     <?php
@@ -20,6 +20,26 @@
       $heading = "Status";
       include 'navbar.php';
     ?>
+
+    <div class="setup_createAccount_header">
+        <h1>Status:</h1>
+    </div>
+
+
+    <!--Table for displaying the status's of all the other pages-->
+    <table border="1" id="status_table">
+        <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+        </tr>
+    </table>
+
 
 </div>
   <?php include "footer.php" ?>
