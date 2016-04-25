900fa35efa1d287435cb186bf8eaf86733ea2ec0
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-04-25 08:55:26 -0600

    removed siteName page, didn't need it

--- siteName.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: text/json');
-// page that sets the siteName for our project 3
-$siteName = array("siteName" => "tyru5Dan::PetRescue");
-echo json_encode($siteName);
-
-?>
