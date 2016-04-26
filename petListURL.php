a95028ff390d9fda717fbe42cc4ba4d7f299e907
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-04-26 08:59:51 -0600

    modified permissions on files to work on the department machines and finished the petListURL.php page

--- petListURL.php
+++ petListURL.php
@@ -1,12 +1,60 @@
 <?php
 header('Content-Type: text/json');
-include 'lib/database.php';
 
-$db = new database();
-$animalQuery = $db->query("SELECT pet_name,pet_species,pet_breed,id FROM animals");
-$res = $animalQuery->fetchAll();
+$animalArray = array();
+$i = 0;
 
-echo json_encode($res);
+$animalArray[$i++] = array(
+
+  "petName" =>  "Lilly",
+  "petKind" =>  "Mammal",
+  "breed"   =>  "Pitbull",
+  "datePosted" => "04/29/2016",
+  "imageURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getImage.php?petId=1",
+  "petId"   =>  "1",
+  "descURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getDesc.php?petId=1",
+
+);
+
+$animalArray[$i++] = array(
+
+  "petName" =>  "Charles",
+  "petKind" =>  "Mammal",
+  "breed"   =>  "cutest kittie",
+  "datePosted" => "04/25/2016",
+  "imageURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getImage.php?petId=2",
+  "petId"   =>  "2",
+  "descURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getDesc.php?petId=2",
+
+);
+
+$animalArray[$i++] = array(
+
+  "petName" =>  "Snuzzles",
+  "petKind" =>  "Mammal",
+  "breed"   =>  "cuter kittie",
+  "datePosted" => "04/20/2016",
+  "imageURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getImage.php?petId=3",
+  "petId"   =>  "3",
+  "descURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getDesc.php?petId=3",
+
+
+);
+
+$animalArray[$i++] = array(
+
+  "petName" =>  "Lucas",
+  "petKind" =>  "na",
+  "breed"   =>  "na",
+  "datePosted" => "04/30/2038", // end of the world!
+  "imageURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getImage.php?petId=4",
+  "petId"   =>  "4",
+  "descURL" =>  "https://www.cs.colostate.edu/~tmalmst/CT310-P3/getDesc.php?petId=4",
+
+);
+
+// print_r($animalArray);
+echo json_encode($animalArray);
 
 
 ?>
