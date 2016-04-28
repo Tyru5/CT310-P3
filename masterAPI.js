800dfa56f6db9fede01c50c23dacc9af5a313f61
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-04-27 20:10:38 -0600

    got the status to finally work

--- masterAPI.js
+++ masterAPI.js
@@ -13,14 +13,14 @@ function ajax_request(){
     // doing this so that my code can work across all (most) platforms:
 
     // current, up to date way of creating an ajax object.
+    try{
+        var request = new XMLHttpRequest();
+    }catch(exception){
+        // using IE6+: <-- why microsoft, why?
         try{
-            var request = new XMLHttpRequest();
-        }catch(exception){
-            // using IE6+: <-- why microsoft, why?
+            request = new ActiveXObject("Msxml2.XMLHTTP");
+        }catch(exception2){
             try{
-                request = new ActiveXObject("Msxml2.XMLHTTP");
-            }catch(exception2){
-                try{
                 // using IE5 <-- again, why?
                 request = new ActiveXObject("Microsoft.XMLHTTP");
             }catch(exception3){
@@ -33,59 +33,64 @@ function ajax_request(){
 
 
 function getStatus() {
-	var status;
-	http = new ajax_request();
-	http.open("GET", vurl, true);
-	http.onreadystatechange = function() {
-		if (http.readyState == 4 && http.status == 200) {
-			status = JSON.parse( http.responseText );
-			// adding my site into the array:
-			status[0] = our_siteJSONobj;
-			console.log( status );
-			statusToTable( status );
-		}
+    var status;
+    http = new ajax_request();
+    http.open("GET", vurl, true);
+    http.onreadystatechange = function() {
+	if (http.readyState == 4 && http.status == 200) {
+	    status = JSON.parse( http.responseText );
+	    // adding my site into the array:
+	    status[0] = our_siteJSONobj;
+	    console.log( status );
+	    statusToTable( status );
 	}
-	http.send( null );
+    }
+    http.send( null );
 
 }
 
 function statusToTable(status) {
-	var tab = document.getElementById('status_table');
-	var i = tab.rows.length;
-	// console.log("The length of the status is = " + status.length);
-	for (j = 0; j < status.length; j++) {
-		var rt = "<tr> <td>" + status[j].siteName + "</td> <td>" + status[j].awakeURL
-				+ "</td> <td>" + status[j].petsListURL + "</td> <td class=\"statusColor\"></td></tr>";
-		var rr = tab.insertRow(i);
-		rr.innerHTML = rt;
-		update_status( status[j].awakeURL, i );
-	}
+    var tab = document.getElementById('status_table');
+    var i = tab.rows.length;
+    // console.log("The length of the status is = " + status.length);
+    for (j = 0; j < status.length; j++) {
+	var rt = "<tr> <td>" + status[j].siteName + "</td> <td>" + status[j].awakeURL
+	    + "</td> <td>" + status[j].petsListURL + "</td> <td class=\"statusColor\"></td></tr>";
+	var rr = tab.insertRow(i);
+	rr.innerHTML = rt;
+	update_status( status[j].awakeURL, i );
+    }
 }
 
 function update_status( siteStatus , numRows){ // this will do a cross-domain request... need to do something about this.. issues will arise.
-	request_awake = new ajax_request();
-	console.log(request_awake);
-	var awake_url = siteStatus;
-	var response;
-	for(j = 0; j < numRows; j++){
+    request_awake = new ajax_request();
+    console.log(request_awake);
+    var awake_url = siteStatus;
+    var response;
+    for(j = 0; j < numRows; j++){
 	request_awake.open("GET", awake_url, true);
 	request_awake.onreadystatechange = function() {
-		if (request_awake.readyState == 4 && request_awake.status == 200) {
-			response = JSON.parse( request_awake.responseText );
-			console.log( response );
-		}
+	    if (request_awake.readyState == 4 && request_awake.status == 200) {
+		response = JSON.parse( request_awake.responseText );
+		console.log( response.status );
+		show_color( response.status);
+	    }
 	}
 	request_awake.send( null );
-		if( response == "up"){
-			$('.statusColor').css('background-color', 'green');
-		}else if( response == "down" ){
-			$('.statusColor').css('background-color', 'yellow');
-		}else{
-			$('.statusColor').css('background-color', 'red');
-		}
-	}
+    }
+}
+
+
+function show_color(some_response){
+    if( some_response  == "up"){
+	$('.statusColor').css('background-color', 'green');
+    }else if( some_response  == "down" ){
+	$('.statusColor').css('background-color', 'yellow');
+    }else{
+	$('.statusColor').css('background-color', 'red');
+    }
 }
 
 function masterAPI() {
-	getStatus();
+    getStatus();
 }
