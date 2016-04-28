89a33a49a0776e7aea7656da8103f301dfd1d435
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-04-28 12:24:33 -0600

    updated our site status

--- awake.php
+++ awake.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/json');
 
-$siteStatus = array("status" => "down"); // this is because we are still working on the site.
+$siteStatus = array("status" => "up"); // this is because we are still working on the site.
 echo json_encode($siteStatus);
 
 ?>
