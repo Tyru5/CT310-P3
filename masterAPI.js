2906e291d55f4e04a3dc0773ab9aca601d2215b2
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-05-02 18:43:38 -0600

    something

--- masterAPI.js
+++ masterAPI.js
@@ -64,7 +64,7 @@ function update_status( siteStatus, cr ){
   $.ajax({
       type: "GET",
       url: siteStatus,
-      // dataType: "json", ???????????????
+      dataType: "json",
       success: function (response) {
         // console.log(typeof(response.status));
         show_color( response.status, cr );
